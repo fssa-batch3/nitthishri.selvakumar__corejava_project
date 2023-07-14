1dae7ffedf5abcbd1f9a2740992cd3ced06059ec
Nitthishri-0104 <116252627+Nitthishri-0104@users.noreply.github.com>
2023-07-14 16:52:53 +0530

    after practice1

--- nitthishri_core_java/src/main/java/day7/practice/RemoveDuplicate.java
+++ nitthishri_core_java/src/main/java/day7/practice/RemoveDuplicate.java
@@ -1,5 +1,24 @@
 package day7.practice;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemoveDuplicate{
+	public static void main(String[] args) {
+		
+        ArrayList cityList = new ArrayList();
+        cityList.add("Chennai");
+        cityList.add("Bangalore");
+        cityList.add("Mumbai");
+        cityList.add("Chennai");
+        cityList.add("Mumbai");
+        
+        Set<String> city = new HashSet<String>(cityList);
+        city.addAll(cityList);
+        System.out.println(city);
+         
+	}
+	
 
 }
